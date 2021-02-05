f7faddd850938241f322ef0afa7c0c7006a8fe0d
id-zonex <idzone027@gmail.com>
2021-02-05 20:50:10 +0200

    Update spawner

--- Memory/Assets/Scripts/SceneControler.cs
+++ Memory/Assets/Scripts/SceneControler.cs
@@ -8,15 +8,13 @@ public class SceneControler : MonoBehaviour
 
     public GameObject[] CardPrefabs;
     private List<GameObject> Cards;
-    
+
     public int xCardCount;
     public int yCardCount;
 
-    private int cardsCount
-    {
-        get { return xCardCount * yCardCount; }
+    public int cardsCount { get { return xCardCount * yCardCount; } }
 
-    }
+    public bool canReveal { get { return _secondRevealed == null; } }
 
     public int xOffSet;
     public int yOffSet;
@@ -25,31 +23,30 @@ public class SceneControler : MonoBehaviour
     private Card _secondRevealed;
 
     private int _store;
-
-    public bool canReveal 
-    { 
-        get { return _secondRevealed == null; } 
-    }
-
+    
     private Camera camera;
 
     void Start()
     {
         camera = Camera.main;
         Cards = RandomlyFillList(CardPrefabs);
-        float yOff = 0;
+       
 
+        float yOff = 0;
         float width = xCardCount * xOffSet;
         float heigth = yCardCount * yOffSet;
+
         var index = 0;
+
         for (int y = 0; y < yCardCount; y++)
         {
-
             yOff += yOffSet;
             float Xoff = 0;
+
             for (int x = 0; x < xCardCount; x++)
             {
                 Xoff += xOffSet;
+
                 try
                 {
                     var card = Instantiate(Cards[index], new Vector3(Xoff, yOff, 10), Quaternion.identity);
@@ -57,17 +54,14 @@ public class SceneControler : MonoBehaviour
                 catch (System.Exception)
                 {
                     break;
-                    throw;
                 }
                 index++;
             }
 
         }
-        print(new Vector3((width / 2), (heigth / 2) + 2.8f));
-        camera.transform.position = new Vector3((width / 2), (heigth / 2) + 2.8f);
+        camera.transform.position = new Vector3(Mathf.Floor(width / 2), Mathf.Floor(heigth / 2) + 2.79f);
         
-        camera.orthographicSize = Mathf.Floor(heigth / 2);
-        print(camera.orthographicSize);
+        camera.orthographicSize = Mathf.Floor((width + heigth) / 2) * 0.4f;
 
     }
 
